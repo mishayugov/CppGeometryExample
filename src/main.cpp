b1ee908725376af219ee0e1b9413babc59e12c58
Michael Yugov <mishayugov@outlook.com>
2023-01-08 16:26:01 +0500

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <imgui.h>
 #include <fstream>
 
+#include <bits/stdc++.h>
+
 // первое множество
 static const int SET_1 = 0;
 // второе множество
@@ -25,37 +27,53 @@ static const char OUTPUT_PATH[255] = "D:/Programming/Files/out.txt";
 // путь к файлу ввода
 static const char INPUT_PATH[255] = "D:/Programming/Files/in.txt";
 
-// Точка
-struct Point {
-    // положение
+// круг
+struct Circle {
+    // положение центра
     sf::Vector2i pos;
-    // номер множества
-    int setNum;
+    // положение точки на окружности
+    sf::Vector2i pos2;
 
     // конструктор
-    Point(const sf::Vector2i &pos, int setNum) : pos(pos), setNum(setNum) {
-    }
-
-    // получить случайную точку
-    static Point randomPoint() {
-        return Point(sf::Vector2i(
-                             rand() % WINDOW_SIZE_X,
-                             rand() % WINDOW_SIZE_Y),
-                     rand() % 2
-        );
+    Circle(const sf::Vector2i &pos, const sf::Vector2i &pos2) : pos(pos), pos2(pos2) {
     }
 };
 
-// динамический список точек
-std::vector<Point> points;
+// динамический список кругов
+std::vector<Circle> circles;
+
+// угол
+struct Angle {
+    // положение центра
+    sf::Vector2i pos1;
+    // положение твух точек
+    sf::Vector2i pos2;
+    sf::Vector2i pos3;
+
+    // конструктор
+    Angle(const sf::Vector2i &pos1, const sf::Vector2i &pos2,const sf::Vector2i &pos3) : pos1(pos1), pos2(pos2),pos3(pos3) {
+    }
+};
+
+// динамический список углов
+std::vector<Angle> angles;
+
 
 // цвет фона
 static sf::Color bgColor;
 // значение цвета по умолчанию
 float color[3] = {0.12f, 0.12f, 0.13f};
 
-// буфер, хранящий координаты последней добавленной вершины
-int lastAddPosBuf[2] = {0, 0};
+// буфер, хранящий координаты последней добавленной точки правой кнопкой
+int fAddPosBufR[2] = {-1, 0};
+// буфер, хранящий координаты предпоследней добавленной точки правой кнопкой
+int sAddPosBufR[2] = {-1, 0};
+// буфер, хранящий координаты предпредпоследней добавленной точки правой кнопкой
+int tAddPosBufR[2] = {-1, 0};
+// буфер, хранящий координаты предпредпоследней добавленной точки левой кнопкой
+int fAddPosBufL[2] = {-1, 0};
+// буфер, хранящий координаты предпредпоследней добавленной точки левой кнопкой
+int sAddPosBufL[2] = {-1, 0};
 
 // буфер кол-ва случайных точек
 int lastRandoCntBuf[1] = {10};
@@ -69,41 +87,26 @@ static void setColor(float *pDouble) {
 }
 
 
-// добавить заданное кол-во случайных точек
-void randomize(int cnt) {
-    for (int i = 0; i < cnt; i++) {
-        points.emplace_back(Point::randomPoint());
-    }
-}
 
 // запись в файл
 void saveToFile() {
     // открываем поток данных для записи в файл
     std::ofstream output(OUTPUT_PATH);
 
-    // перебираем точки
-    for (auto point: points) {
-        // выводим через пробел построчно: x-координату, y-координату и номер множества
-        output << point.pos.x << " " << point.pos.y << " " << point.setNum << std::endl;
+    // перебираем окружности
+    for (auto circle: circles) {
+        output << circle.pos.x << " " << circle.pos.y << " " << circle.pos2.x <<" "<<circle.pos2.y <<std::endl;
     }
-
+    // перебираем углы
+    for (auto angle: angles) {
+        output << angle.pos1.x << " " << angle.pos1.y << " " << angle.pos2.x << " " << angle.pos2.y<< " "<< angle.pos3.x << " " << angle.pos3.y << std::endl;
+    }
     // закрываем
     output.close();
 }
 
 // решение задачи
 void solve() {
-    // у совпадающих по координатам точек меняем множество на SET_CROSSED
-    for (int i = 0; i < points.size(); i++)
-        for (int j = i + 1; j < points.size(); j++)
-            if (points[i].pos == points[j].pos)
-                points[i].setNum = points[j].setNum = SET_CROSSED;
-
-    // у всех точек, у которых множество не SET_CROSSED, задаём множество SET_SINGLE
-    for (auto &point: points)
-        if (point.setNum != SET_CROSSED)
-            point.setNum = SET_SINGLE;
-
 }
 
 // загрузка из файла
@@ -111,15 +114,30 @@ void loadFromFile() {
     // открываем поток данных для чтения из файла
     std::ifstream input(INPUT_PATH);
     // очищаем массив точек
-    points.clear();
+    circles.clear();
+    angles.clear();
     // пока не достигнут конец файла
     while (!input.eof()) {
-        int x, y, s;
-        input >> x; // читаем x координату
-        input >> y; // читаем y координату
-        input >> s; // читаем номер множества
-        // добавляем в динамический массив точку на основе прочитанных данных
-        points.emplace_back(Point(sf::Vector2i(x, y), s));
+        int x, y, rad, L,x1,x2,x3,y1,y2,y3;
+        input >> L;
+        if (L == 0) {
+            input >> x; // читаем x координату
+            input >> y; // читаем y координату
+            input >> x1; // читаем x координату точки на окружности
+            input>>y1; // читаем y координату точки на окружности
+            // добавляем в динамический массив окружность на основе прочитанных данных
+            circles.emplace_back(Circle(sf::Vector2i(x, y), sf::Vector2i(x1, y1)));
+        }
+        if (L != 0) {
+            input >> x1; // читаем x координату
+            input >> y1; // читаем y координату
+            input >> x2; // читаем x координату
+            input >> y2; // читаем y координату
+            input >> x3; // читаем x координату
+            input >> y3; // читаем y координату
+            // добавляем в динамический массив угол на основе прочитанных данных
+            angles.emplace_back(Angle(sf::Vector2i(x1, y1), sf::Vector2i(x2, y2), sf::Vector2i(x3, y3)));
+        }
     }
     // закрываем файл
     input.close();
@@ -140,6 +158,31 @@ void ShowBackgroundSetting() {
     }
 }
 
+// сумма векторов
+static sf::Vector2i sum(sf::Vector2i a, sf::Vector2i b) {
+    return {a.x + b.x, a.y + b.y};
+}
+
+// разность векторов
+static sf::Vector2i subtract(sf::Vector2i a, sf::Vector2i b) {
+    return {a.x - b.x, a.y - b.y};
+}
+
+// умножение вектора на число
+static sf::Vector2f mul(sf::Vector2f a, float b) {
+    return {a.x * b, a.y * b};
+}
+
+// преобразование целочисленного вектора в вещественный
+static sf::Vector2f toFloat(sf::Vector2i v) {
+    return {(float)v.x, (float)v.y};
+}
+
+// преобразование вещественного вектора в целочисленный
+static sf::Vector2i toInt(sf::Vector2f v) {
+    return {(int) v.x, (int) v.y};
+}
+
 // рисование задачи на невидимом окне во всё окно приложения
 void RenderTask() {
     // задаём левый верхний край невидимого окна
@@ -153,30 +196,79 @@ void RenderTask() {
     // получаем список примитивов, которые будут нарисованы
     auto pDrawList = ImGui::GetWindowDrawList();
 
-    // перебираем точки из динамического массива точек
-    for (auto point: points) {
+    // перебираем окружности из динамического массива окружностей
+    for (auto circle: circles) {
         ImColor clr;
-        // Устанавливаем цвет по номеру множества
-        switch (point.setNum) {
-            case SET_1:
-                clr = ImColor(200, 100, 150);
-                break;
-            case SET_2:
-                clr = ImColor(100, 200, 150);
-                break;
-            case SET_CROSSED:
-                clr = ImColor(100, 150, 200);
-                break;
-            case SET_SINGLE:
-                clr = ImColor(150, 200, 100);
-                break;
-        }
+        clr = ImColor(200, 100, 150);
+
         // добавляем в список рисования круг
-        pDrawList->AddCircleFilled(
-                sf::Vector2i(point.pos.x, point.pos.y),
-                3,
+        pDrawList->AddCircle(
+                sf::Vector2i(circle.pos.x, circle.pos.y),
+                pow(pow(circle.pos2.x-circle.pos.x,2)+pow(circle.pos2.y-circle.pos.y,2),0.5),
                 clr,
-                20
+                100
+        );
+    }
+    // перебираем углы из динамического массива углоы
+    for (auto angle: angles) {
+        ImColor clr;
+        clr = ImColor(10, 100, 150);
+
+        // рисуем угол
+        // вершина угла
+        sf::Vector2i pointA = angle.pos1;
+        // опорные точки
+        sf::Vector2i pointB = angle.pos2;
+        sf::Vector2i pointC = angle.pos3;
+
+        // определяем вектор смещения
+        sf::Vector2i AB = subtract(pointB, pointA);
+        sf::Vector2i AC = subtract(pointC, pointA);
+
+
+        // получаем максимальную длину отрезка на экране, как длину диагонали экрана
+        float maxDistance = std::sqrt(WINDOW_SIZE_X * WINDOW_SIZE_X + WINDOW_SIZE_Y * WINDOW_SIZE_Y);
+
+        // получаем новые точки для рисования, которые гарантируют, что линия
+        // будет нарисована до границ экрана
+        sf::Vector2i renderPointB = sum(pointA, toInt(mul(toFloat(AB), maxDistance)));
+        sf::Vector2i renderPointC = sum(pointA, toInt(mul(toFloat(AC), maxDistance)));
+
+
+        // рисуем линию
+        pDrawList->AddLine(
+                pointA,
+                renderPointB,
+                clr,
+                0.5f
+        );
+
+
+        // рисуем линию
+        pDrawList->AddLine(
+                pointA,
+                renderPointC,
+                clr,
+                0.5f
+        );
+
+        // рисуем точку
+        pDrawList->AddCircleFilled(
+                pointA,
+                3,
+                clr
+        );
+        // рисуем точку
+        pDrawList->AddCircleFilled(
+                pointB,
+                3,
+                clr
+        );
+        // рисуем точку
+        pDrawList->AddCircleFilled(
+                pointC,
+                3,
+                clr
         );
     }
     // заканчиваем рисование окна
@@ -192,7 +284,7 @@ void ShowAddElem() {
 
 
     // Инструмент выбора цвета
-    if (ImGui::DragInt2("Coords", lastAddPosBuf, 0.5f, 0, std::min(WINDOW_SIZE_X, WINDOW_SIZE_Y))) {
+    if (ImGui::DragInt2("Coords", fAddPosBufL, 0.5f, 0, std::min(WINDOW_SIZE_X, WINDOW_SIZE_Y))) {
         // никаких действий не требуется, достаточно
         // тех изменений буфера, которые imGui выполняет
         // автоматически
@@ -201,9 +293,12 @@ void ShowAddElem() {
     // фиксируем id равный 0 для первого элемента
     ImGui::PushID(0);
     // если нажата кнопка `Set 1`
-    if (ImGui::Button("Set 1"))
-        // добавляем то добавляем в список точку, принадлежащую первому множеству
-        points.emplace_back(Point(sf::Vector2i(lastAddPosBuf[0], lastAddPosBuf[1]), SET_1));
+    if (ImGui::Button("Set 1") and sAddPosBufL[0]!=-1) {
+        // добавляем в список окружность
+        circles.emplace_back(Circle(sf::Vector2i(fAddPosBufL[0], fAddPosBufL[1]), sf::Vector2i(sAddPosBufL[0], sAddPosBufL[1])));
+        fAddPosBufL[0]=-1;
+        sAddPosBufL[0]=-1;
+    }
     // восстанавливаем буфер id
     ImGui::PopID();
 
@@ -212,39 +307,17 @@ void ShowAddElem() {
     // задаём id, равный одному
     ImGui::PushID(1);
     // если нажата кнопка `Set 2`
-    if (ImGui::Button("Set 2"))
-        // добавляем то добавляем в список точку, принадлежащую второму множеству
-        points.emplace_back(Point(sf::Vector2i(lastAddPosBuf[0], lastAddPosBuf[1]), SET_2));
-    // восстанавливаем буфер id
-    ImGui::PopID();
-}
-
-// панель добавления случайных точек
-void ShowRandomize() {
-    // если не раскрыта панель `Randomize`
-    if (!ImGui::CollapsingHeader("Randomize"))
-        // заканчиваем выполнение
-        return;
-
-    // первый элемент в строке
-    ImGui::PushID(0);
-
-    // Инструмент выбора кол-ва
-    if (ImGui::DragInt("Count", lastRandoCntBuf, 0.1, 0, 100)) {
-
+    if (ImGui::Button("Set 2"),tAddPosBufR[0]!=-1) {
+        // добавляем в список угол
+        angles.emplace_back(Angle(sf::Vector2i(fAddPosBufR[0], fAddPosBufR[1]), sf::Vector2i(sAddPosBufR[0], sAddPosBufR[1]),sf::Vector2i(tAddPosBufR[0], tAddPosBufR[1])));
+        tAddPosBufR[0]=-1;
+        fAddPosBufR[0]=-1;
+        sAddPosBufR[0]=-1;
     }
     // восстанавливаем буфер id
     ImGui::PopID();
-    // следующий элемент будет на той же строчке
-    ImGui::SameLine();
-    // второй элемент
-    ImGui::PushID(1);
-    // создаём кнопку добавления
-    if (ImGui::Button("Add"))
-        // по клику добавляем заданное число случайных точек
-        randomize(lastRandoCntBuf[0]);
-    ImGui::PopID();
 }
+
 
 
 // работа с файлами
@@ -300,8 +373,9 @@ void ShowSolve() {
 
     // создаём кнопку очистки
     if (ImGui::Button("Clear")) {
-        // удаляем все точки
-        points.clear();
+        // удаляем всё
+        circles.clear();
+        angles.clear();
     }
     // восстанавливаем буфер id
     ImGui::PopID();
@@ -315,7 +389,7 @@ void ShowHelp() {
     // первый заголовок
     ImGui::Text("ABOUT THIS DEMO:");
     // первый элемент списка
-    ImGui::BulletText("Author Ivanov Ivan 10-1");
+    ImGui::BulletText("Author Yugov Michail 10-1");
     // второй элемент списка
     ImGui::BulletText("Powered by SFML+ImGui");
     // разделитель
@@ -324,10 +398,10 @@ void ShowHelp() {
     // второй заголовок
     ImGui::Text("TASK:");
     // первый элемент списка(многострочный)
-    ImGui::BulletText("Two sets of points are given\n"
-                      "in an integer two-dimensional space.\n"
-                      "It is required to build intersections and\n"
-                      "the difference between these sets.");
+    ImGui::BulletText("A set of circles and a set of angles are given on the plane\n"
+                      "This program is made to find an angle and a circle\n"
+                      "for which the area within their intersection achieves the maximum value\n"
+                      "");
     // разделитель
     ImGui::Separator();
 
@@ -366,14 +440,41 @@ int main() {
             if (event.type == sf::Event::MouseButtonPressed) {
                 // если мышь не обрабатывается элементами imGui
                 if (!ImGui::GetIO().WantCaptureMouse) {
-                    // меняем координаты последней добавленной точки
-                    lastAddPosBuf[0] = event.mouseButton.x;
-                    lastAddPosBuf[1] = event.mouseButton.y;
                     // если левая кнопка мыши
-                    if (event.mouseButton.button == sf::Mouse::Button::Left)
-                        points.emplace_back(sf::Vector2i(event.mouseButton.x, event.mouseButton.y), SET_1);
-                    else
-                        points.emplace_back(sf::Vector2i(event.mouseButton.x, event.mouseButton.y), SET_2);
+                    if (event.mouseButton.button == sf::Mouse::Button::Left) {
+                        if (fAddPosBufL[0]==-1) {
+                            fAddPosBufL[0] = event.mouseButton.x;
+                            fAddPosBufL[1] = event.mouseButton.y;
+                        } else if (sAddPosBufL[0]==-1) {
+                            sAddPosBufL[0] = event.mouseButton.x;
+                            sAddPosBufL[1] = event.mouseButton.y;
+                        }
+                        if (sAddPosBufL[0]!=-1) {
+                            circles.emplace_back(sf::Vector2i(fAddPosBufL[0], fAddPosBufL[1]),
+                                                 sf::Vector2i(sAddPosBufL[0], sAddPosBufL[1]));
+                            fAddPosBufL[0] = -1;
+                            sAddPosBufL[0] = -1;
+                        }
+                    }else if (event.mouseButton.button == sf::Mouse::Button::Right) {
+                        if (fAddPosBufR[0]==-1) {
+                            fAddPosBufR[0] = event.mouseButton.x;
+                            fAddPosBufR[1] = event.mouseButton.y;
+                        } else if ( sAddPosBufR[0]==-1){
+                            sAddPosBufR[0] = event.mouseButton.x;
+                            sAddPosBufR[1] = event.mouseButton.y;
+                        } else if (tAddPosBufR[0]==-1){
+                            tAddPosBufR[0] = event.mouseButton.x;
+                            tAddPosBufR[1] = event.mouseButton.y;
+                        }
+                        if (tAddPosBufR[0]!=-1) {
+                            angles.emplace_back(sf::Vector2i(fAddPosBufR[0], fAddPosBufR[1]),
+                                                sf::Vector2i(sAddPosBufR[0], sAddPosBufR[1]),
+                                                sf::Vector2i(tAddPosBufR[0], tAddPosBufR[1]));
+                            fAddPosBufR[0] = -1;
+                            sAddPosBufR[0] = -1;
+                            tAddPosBufR[0] = -1;
+                        }
+                    }
                 }
             }
         }
@@ -395,7 +496,7 @@ int main() {
         // ручное добавление элементов
         ShowAddElem();
         // добавление случайных точек
-        ShowRandomize();
+
         // работа с файлами
         ShowFiles();
         // решение задачи
